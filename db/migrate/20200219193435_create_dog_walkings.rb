4baf586bf80dd340b9e1452b773517afa7aac114
Andre Protti <andrejardimprotti@gmail.com>
2020-02-19 16:42:18 -0300

    Creates the DogWalking model

---
+++ db/migrate/20200219193435_create_dog_walkings.rb
@@ -0,0 +1,17 @@
+class CreateDogWalkings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :dog_walkings do |t|
+      t.integer :status
+      t.datetime :scheduled_date
+      t.float :price
+      t.integer :duration
+      t.float :latitude
+      t.float :longitude
+      t.string :pets
+      t.time :begins
+      t.time :ends
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_02_19_193435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "dog_walkings", force: :cascade do |t|
+    t.integer "status"
+    t.datetime "scheduled_date"
+    t.float "price"
+    t.integer "duration"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "pets"
+    t.time "begins"
+    t.time "ends"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
